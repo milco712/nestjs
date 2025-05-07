c5e7b56a6acbab545610275d4b3946b8147f42ce
eunji <helena712@gmail.com>
2025-05-08 00:56:29 +0900

    patch까지

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,6 +1,7 @@
 import {Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 import {UsersModel} from "../../users/entities/users.entity";
 import {BaseModel} from "../../common/entities/base.entity";
+import {IsString} from "class-validator";
 
 @Entity()
 export class PostsModel extends BaseModel{
@@ -8,9 +9,15 @@ export class PostsModel extends BaseModel{
     author: UsersModel;
 
     @Column()
+    @IsString({
+        message: 'title은 string 타입을 입력해야합니다.'
+    })
     title: string;
 
     @Column()
+    @IsString({
+        message: 'content는 string 타입을 입력해야합니다.'
+    })
     content: string;
 
     @Column()
